6bb9d6ff7bf84cd258089690d475f5dc8a31b43d
bckwon1234 <kwon33492@gmail.com>
2026-01-13 10:25:35 +0900

    Add files via upload

--- app.py
+++ app.py
@@ -165,4 +165,5 @@ def health():
     return "OK", 200
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    port = int(os.environ.get("PORT", 10000))  # Render가 요구하는 포트
+    app.run(host='0.0.0.0', port=port, debug=False)
